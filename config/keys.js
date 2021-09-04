b021816eff4dc933c79b3589349a6f47c0a98ce6
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-09-04 20:40:10 +0100

    logica jwt

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  secretOrKey: "$#ls/kdchpwofhow304o0^&&*_82203-21=12329023u4y32h#?>!@#$%7^",
+};
---
+++ config/passport.js
@@ -0,0 +1,26 @@
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+
+const User = require("../models/user");
+const Keys = require("./keys");
+
+module.exports = function (passport) {
+  let opts = {};
+
+  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
+  opts.secretOrKey = Keys.secretOrKey;
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      User.findById(jwt_payload.id, (error, user) => {
+        if (error) {
+          return done(error, false);
+        }
+        if (user) {
+          return done(null, user);
+        } else {
+          return done(null, false);
+        }
+      });
+    })
+  );
+};
