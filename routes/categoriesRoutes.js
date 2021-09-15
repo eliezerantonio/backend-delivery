8c3f45d531112ae37e62413af6bd09f4a648b254
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-09-15 01:20:02 +0100

    rota buscar todos

--- routes/categoriesRoutes.js
+++ routes/categoriesRoutes.js
@@ -3,6 +3,15 @@ const CategoriesController = require("../controllers/categoriesController");
 const passport = require("passport");
 
 module.exports = (app) => {
+  /*
+  GET ALL
+  */
+  app.post(
+    "/api/categories/getall",
+    passport.authenticate("jwt", { session: false }),
+    CategoriesController.getAll
+  );
+
   /**
      * 
     POST
